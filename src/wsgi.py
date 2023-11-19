76efb0f05078c997b84bf98c8d70df70bf3d4199
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-11-19 22:36:47 +0200

    fix: pylint fixes

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,14 @@
+"""
+Defines how the app is run fly.io.
+In general you should not need to touch this if CI/CD works.
+"""
+
+# these pylint warnings are ignored because these errors
+# are not "correct". This file is launced by gunicorn
+# and not with python as usually, therefore the errors
+# don't apply
+
+# pylint: disable=import-error, no-name-in-module
 from src.app import app
 
 if __name__ == "__main__":
