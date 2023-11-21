f509735ff7d94272d33d6248b23c247e11740f2c
Antti Aarnio <laborantti.antti@gmail.com>
2023-11-21 10:59:55 +0200

    replaced DatabaseError with RuntimeError, problematic for pylint

--- src/app.py
+++ src/app.py
@@ -4,14 +4,13 @@ Defines the Flask applications dependencies and database connection.
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import DatabaseError
 
 if os.environ.get('ENVIRONMENT') == 'production':
     sql_url = os.environ.get('POSTGRES_URL') or None
 else:
     sql_url = os.environ.get('TEST_POSTGRES_URL') or None
 if sql_url is None:
-    raise DatabaseError("No SQL URL was found. Connection cannot be made.")
+    raise RuntimeError("No SQL URL was found. Connection cannot be made.")
 
 app = Flask(__name__)
 app.config["DATABASE_URI"] = sql_url
