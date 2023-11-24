bcff01de8107d686ee30918660858560fe40a951
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-11-24 11:50:45 +0200

    added temporary intercommunication between add and view ref

--- src/utils/reference_handler.py
+++ src/utils/reference_handler.py
@@ -6,10 +6,16 @@ Module for handling references
 # pylint: disable=import-error, no-name-in-module
 from src.references.reference import Reference
 
+references = []
+
 def create_reference(author: str, title: str, year: str):
     """
     Module function for creating Reference object
     """
     ref = Reference(author, title, year)
+    references.append(ref)
 
     return ref
+
+def get_references():
+    return references
