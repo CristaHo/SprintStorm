8fbdd77bbdb811cceaa7983a0b3352dc0ceab52e
Crista <crista.holmstrom@outlook.com>
2023-11-24 10:57:44 +0200

    Index_bp lisätty

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ Defines the Flask applications dependencies and database connection.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from views.index import index_bp
 from views.add_reference import add_reference_bp
 from views.view_reference import view_reference_bp
 from utils.environment import read_postgres_url
@@ -16,6 +17,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = sql_url
 db = SQLAlchemy(app)
 
+app.register_blueprint(index_bp)
 app.register_blueprint(add_reference_bp)
 app.register_blueprint(view_reference_bp)
 
