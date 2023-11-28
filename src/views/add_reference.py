4bc8f2e6dd5d75c65f053dd80fadec36f698097d
Crista <crista.holmstrom@outlook.com>
2023-11-28 13:16:30 +0200

    Choosing reference type

--- src/views/add_reference.py
+++ src/views/add_reference.py
@@ -6,6 +6,17 @@ from flask import Blueprint, request, render_template
 from src.utils.reference_handler import reference_handler
 
 add_reference_bp = Blueprint('add_reference', __name__)
+choose_reference_bp = Blueprint('choose-reference', __name__)
+
+@choose_reference_bp.route("/choose_reference", methods=["GET", "POST"])
+def choose_reference():
+    if request.method == "GET":
+        return render_template("add_reference.html")
+    if request.method == "POST":
+        choice = request.args["ref"]
+
+        return render_template("add_reference.html", choice)
+
 
 @add_reference_bp.route("/add_reference", methods=["GET", "POST"])
 def add_reference():
