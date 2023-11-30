88b728a96cbf938fcc91eb03acef37d56ec22202
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-11-30 23:57:10 +0200

    implemented tests and fixed database.article

--- src/db/article.py
+++ src/db/article.py
@@ -4,14 +4,22 @@ Handles article databse
 
 from sqlalchemy import text
 from src.utils.database import db
+from src.references.article import Article
 
 def get_all():
     """Gets all articles from database"""
     sql = text("SELECT * FROM article")
 
-    result = db.session.execute(sql)
+    result = db.session.execute(sql).fetchall()
+    if result:
+        articles = []
+        for item in result:
+            articles.append(Article(parse_fetchall(item)))
+        return articles
 
-    return result.fetchall()
+    return None
+
+
 
 def insert_one(ref):
     """Inserts one article into database"""
@@ -22,3 +30,16 @@ def insert_one(ref):
                              "journal":ref["journal"], "volume":ref["volume"],
                              "pages":ref["pages"]})
     db.session.commit()
+
+def parse_fetchall(rows):
+    fields = {
+            "key": rows[1],
+            "author": rows[2],
+            "title": rows[3],
+            "year": rows[4],
+            "journal": rows[5],
+            "volume": rows[6],
+            "pages": rows[7]
+            }
+
+    return fields
---
+++ tests/database_tests/article_test.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+
+from src.app import app
+from src.references.article import Article
+
+class ArticleDatabaseTest(TestCase):
+    def setUp(self):
+        with app.app_context():
+            from src.utils.database import reset_database
+            reset_database()
+
+    def test_article_get_all_returns_None_if_no_articles_in_database(self):
+        with app.app_context():
+            from src.db import article
+            result = article.get_all()
+        self.assertEqual(result, None)
+
+    def test_article_insert_one_with_correct_object_is_correctly_saved_to_db(self):
+        test_article = {
+            "author": "Me",
+            "title": "My best book", 
+            "year": 2023,
+            "journal": "Mitä tä tarkottaa :D", 
+            "volume": 3,
+            "pages": "200-300"
+        }
+
+        with app.app_context():
+            from src.db import article
+            pre_result = article.get_all()
+            article.insert_one(test_article)
+            result = article.get_all()
+
+        if result:
+            self.assertEqual(pre_result, None)
+            self.assertIsInstance(result[0], Article)
+
+            self.assertEqual(result[0].author, "Me")
+            self.assertEqual(result[0].title, "My best book")
+            self.assertEqual(result[0].year, 2023)
+            self.assertEqual(result[0].journal, "Mitä tä tarkottaa :D")
+            self.assertEqual(result[0].volume, 3)
+            self.assertEqual(result[0].pages, "200-300")
+        else:
+            raise AssertionError("No result from database")
+
+    def test_article_get_all_multiple_references_returns_all(self):
+        test_article = {
+            "author": "Me",
+            "title": "My best book", 
+            "year": 2023,
+            "journal": "Mitä tä tarkottaa :D", 
+            "volume": 3,
+            "pages": "200-300"
+        }
+
+        other_test_article = {
+            "author": "You",
+            "title": "Your best book", 
+            "year": 2020,
+            "journal": "Edelleenkään en tiedä", 
+            "volume": 1,
+            "pages": "100-200"
+        }
+
+        with app.app_context():
+            from src.db import article
+            article.insert_one(test_article)
+            article.insert_one(other_test_article)
+            result = article.get_all()
+
+        if result:
+            self.assertEqual(len(result), 2)
+            self.assertEqual(result[0].author, "Me")
+            self.assertEqual(result[1].author, "You")
+        else:
+            raise AssertionError("No result from database")
