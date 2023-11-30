a764b68cf4632820e179ab99cc50eefac8fac095
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-12-01 00:26:08 +0200

    implemented tests and fixed src.db.book

--- src/db/book.py
+++ src/db/book.py
@@ -2,7 +2,9 @@
 Handles book database operations
 """
 from sqlalchemy import text
+
 from src.utils.database import db
+from src.references.book import Book
 
 def get_all():
     """
@@ -10,16 +12,43 @@ def get_all():
     """
     sql = text("SELECT * FROM book")
 
-    result = db.session.execute(sql)
+    result = db.session.execute(sql).fetchall()
+    if result:
+        books = []
+        for item in result:
+            books.append(Book(parse_fetchall(item)))
+        return books
 
-    return result.fetchall()
+    return None
 
 def insert_one(ref):
     """
     Inserts one book into database
     """
-    sql = text("INSERT INTO book (author, title, year, publisher)"
-            " VALUES (:author, :title, :year, :publisher)")
-    db.session.execute(sql, {"author":ref["author"], "title":ref["title"],
-                             "year":ref["year"], "publisher":ref["publisher"]})
+    sql = text("INSERT INTO book (author, title, year, publisher, address)"
+               " VALUES (:author, :title, :year, :publisher, :address)")
+    parsed_reference = {
+        "author": ref["author"],
+        "title": ref["title"],
+        "year": ref["year"],
+        "publisher": ref["publisher"],
+        "address": ref["address"]
+    }
+    db.session.execute(sql, parsed_reference)
     db.session.commit()
+
+def parse_fetchall(rows):
+    """
+    Parses book fetchall from get_all and fits the rows inside an object
+    """
+    # pylint: disable-next=duplicate-code
+    fields = {
+            "key": rows[1],
+            "author": rows[2],
+            "title": rows[3],
+            "year": rows[4],
+            "publisher": rows[5],
+            "address": rows[6],
+            }
+
+    return fields
