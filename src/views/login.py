b252a9399adce40af3eafba3aaf355e6711e621e
Juuso Hyyryläinen <juusohyyrylainen@hotmail.com>
2023-12-07 20:08:01 +0200

    more linting

--- src/views/login.py
+++ src/views/login.py
@@ -17,9 +17,7 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-
         if get_user(username, password):
             return redirect(url_for('index'))
-
         flash("Invalid username or password")
-        return render_template("login.html")
+    return render_template("login.html")
