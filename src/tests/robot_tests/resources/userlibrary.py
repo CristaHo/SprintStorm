2333c0fb62d08dfbd3a455b7f4fe24daf7a098c9
PatrickSalmi <patrick.salmi@helsinki.fi>
2023-12-13 14:45:01 +0200

    removed unused userlibrary from robot tests

--- src/tests/robot_tests/resources/userlibrary.py
+++
@@ -1,7 +0,0 @@
-from src.db.user import get_user_id_by_username, delete_user
-
-class UserLibrary:
-    def delete_user_by_username(self, username):
-        user_id = get_user_id_by_username(username)
-        if user_id:
-            delete_user(user_id)
