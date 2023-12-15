4c3d0750aa6dcf6bab15a50aad40788b47db93a3
Antti Aarnio <laborantti.antti@gmail.com>
2023-12-15 12:36:12 +0200

    fixed removal confirmation by splitting to smaller pieces

--- src/views/verify_removal.py
+++ src/views/verify_removal.py
@@ -7,10 +7,28 @@ from src.app import app
 from src.utils.logging import log
 
 
-@app.route("/verify_removal", methods=["POST"])
-def verify_removal():
+@app.route("/verify_removal/reference", methods=["POST"])
+def verify_reference_removal():
     """
-    Route for verifying reference and category removal.
+    Route for verifying reference removal.
+    """
+    user_id = session.get("uid")
+    if user_id is None:
+        flash("Login needed to view this page")
+        return redirect(url_for('login'))
+    reference_type = request.form["reference_type"]
+    reference_key = request.form["reference_key"]
+    if not (reference_key or reference_type):
+        log.info("Invalid removal requested")
+        return redirect(url_for('view_references'))
+    else:
+        log.info(f"Removal for reference {reference_key} of {reference_type} type in process")
+        return render_template("reference_removal.html", remove_reference=True, reference_type=str(reference_type), reference_key=str(reference_key))
+
+@app.route("/verify_removal/category", methods=["POST"])
+def verify_category_removal():
+    """
+    Route for verifying category removal.
     """
     user_id = session.get("uid")
     if user_id is None:
@@ -18,15 +36,9 @@ def verify_removal():
         return redirect(url_for('login'))
     category_id = request.form["id"]
     if not category_id:
-        reference_type = request.form["reference_type"]
-        reference_key = request.form["reference_key"]
-        log.info(f"key: {reference_key}, type: {reference_type}")
-        if not (reference_type and reference_key):
-            log.info("Invalid removal requested")
-            return redirect(url_for('view_references'))
-        else:
-            log.info(f"Removal for reference {reference_key} of {reference_type} type in process")
-            return render_template("verify_removal.html", remove_reference=True, reference_type=str(reference_type), reference_key=str(reference_key))
+        log.info("Invalid removal requested")
+        return redirect(url_for('add_category'))
     else:
         log.info(f"Removal for category id {category_id} in process")
-        return render_template("verify_removal.html", remove_category=True, category_id=category_id)
+        return render_template("category_removal.html", remove_category=True, category_id=category_id)
+
